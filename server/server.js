888b97abfce0015c1136fd92d2af2b7bdb20ffaa
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-05-30 19:07:56 -0700

    edited path of static

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.use(express.static("../my-app/build"));
+app.use(express.static("my-app/build"));
 
 app.use(session({
   secret: "app",
